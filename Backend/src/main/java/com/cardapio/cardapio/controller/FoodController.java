8baa57d228c9be140a54591c839bae95cb18a683
Victor Lima <victorhenriqu3@outlook.com>
2024-01-13 22:31:20 -0400

    feat: create edit Food by Id method

--- Backend/src/main/java/com/cardapio/cardapio/controller/FoodController.java
+++ Backend/src/main/java/com/cardapio/cardapio/controller/FoodController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,7 +49,7 @@ public class FoodController {
         FoodResponseDTO food = new FoodResponseDTO(foodData.get());
         return new ResponseEntity<>(food, HttpStatus.OK);
       } else {
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
       }
 
     } catch (Exception e) {
@@ -63,6 +64,30 @@ public class FoodController {
       Food foodData = new Food(data);
       repository.save(foodData);
       return new ResponseEntity<>(HttpStatus.CREATED);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @CrossOrigin(origins = "*", allowedHeaders = "*")
+  @PutMapping("/{id}")
+  public ResponseEntity<HttpStatus> editFood(@PathVariable("id") long id, @RequestBody FoodRequestDTO data) {
+    try {
+      Optional<Food> foodData = repository.findById(id);
+
+      if (foodData.isPresent()) {
+        Food _food = foodData.get();
+        _food.setDescription(data.description());
+        _food.setImage(data.image());
+        _food.setTitle(data.title());
+        _food.setPrice(data.price());
+        repository.save(_food);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+
+      } else {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      }
     } catch (Exception e) {
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
